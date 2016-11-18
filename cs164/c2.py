9f979ee8c51a8a863f470e0b7fe5c8194a231058
Armando Gonzalez <agonz060@ucr.edu>
2016-11-18 00:06:52 -0800

    uploading

--- cs164/c2.py
+++ cs164/c2.py
@@ -2,6 +2,7 @@
 import socket
 import sys
 import time
+import getpass
 
 try :
 	s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -16,15 +17,34 @@ ip = socket.gethostbyname('')
 
 s.connect((ip,port))
 
-
+pwMsg1 = "password:"
+pwMsg2 = "Password:"
+menuMsg = "Welcome"
+currentPass = "current"
+newMsg = "new"
+loggingOut = "logging out"
+formatReply = '\n'
 while(1) :
+	userIn = ""
 	# Receive input from server (data,addr)
 	data = s.recv(1024)
 	print(data)
-	
-	userIn = raw_input()	
-	
-	reply = data + userIn
+
+	if loggingOut in data:
+		s.close()
+		break
+	if currentPass in data or newMsg in data:
+		userIn = getpass.getpass("")
+		reply = data + formatReply + userIn
+	elif pwMsg1 in data or pwMsg2 in data:
+		pw = getpass.getpass("")
+		reply = data + pw
+	elif menuMsg in data:
+		userIn = raw_input()
+		reply = data + formatReply + userIn
+	else:
+		userIn = raw_input()	
+		reply = data + userIn
 	
 	# Repond to sender
 	s.sendall(reply)
--- cs164/s.py
+++ cs164/s.py
@@ -33,7 +33,7 @@ inputs = [ server ]
 # output sockets
 outputs = [ ]
 
-password = {'mando':'pass123', 'sam':'s1', 'andrew':'a1', 'tim':'t1'}
+password = {'mando':'m1', 'sam':'s1', 'andrew':'a1', 'tim':'t1'}
 users = ['mando', 'sam', 'andrew', 'tim']
 
 msgQue = {'':''}
@@ -47,7 +47,17 @@ errPass = "Error: wrong password. Enter password: "
 userName = {'':''}
 connectedClients = [ ] 
 
-menu = "Welcome \n **************** \n Select option:\n1. Change password\n2.Log out"
+menuMsg = "Welcome"
+menu = "\tWelcome\n*******************\nSelect option:\n1. Change password\n2.Log out"
+changePswdMsg = "Changing password\n********************\nEnter current password: "
+newPswdMsg = "Enter new password: "
+
+newMsg = "new"
+currentPswd = "current"
+newPassSaved = "New password saved!\n"
+loggingOutMsg = "logging out"
+savedMsg = "saved"
+optionIndex = 5
 
 while inputs :
 	
@@ -61,7 +71,9 @@ while inputs :
 			
 			conn.setblocking(0)
 			inputs.append(conn)
-				
+			#print('s.SockName: ',s.getsockname())
+			#print('conn.sockname: ',conn.getsockname())
+			#print('conn.getpeer: ', conn.getpeername())
 			clientPort = str(conn.getpeername()[1])
 			msgQue[clientPort] = getUserName
 			connectedClients.append(clientPort)
@@ -69,6 +81,8 @@ while inputs :
 			if conn not in outputs:
 				outputs.append(conn)
 		else:
+			#print('s.SockName: ',s.getsockname())
+			#print('s.getpeer: ',s.getpeername())
 			data = s.recv(1024)
 			if data:
 				print('recv: '+data)
@@ -79,26 +93,26 @@ while inputs :
 				passWord = 'password:'
 				passWord2 = 'Password:'
 
-				clientPort = str(conn.getpeername()[1])
+				clientPort = str(s.getpeername()[1])
 				
 				if error in data:
 					print('error in data')
 					if uName in data or uName2 in data:
 						print('in errUserName')
 						usrName = data.split(" ")[6]
-						print('username: ',usrName)
+						#print('username: ',usrName)
 						
 						flag = 1
 			
 						for user in users:
 							if user == usrName:
-								print('client: ',clientPort,' username match: ',user)
-								print('getPass: ',getPass)
+								#print('client: ',clientPort,' username match: ',user)
+								#print('getPass: ',getPass)
 								msgQue[clientPort] = getPass
 								userName[clientPort] = user
 								
 								if s not in outputs:
-									print('adding s to outputs')
+									#print('adding s to outputs')
 									outputs.append(s)
 								flag = 0
 								break
@@ -106,40 +120,46 @@ while inputs :
 							print('errorUserName') 
 							msgQue[clientPort] = errUserName
 							if s not in outputs:
-								print('adding s to outputs')
+								print('errorUName: adding s to outputs')
 								outputs.append(s)
 					
 					elif passWord in data or passWord2 in data:
 						print('in errPass')
 					
-						password = data.split(" ")[5]
-						print('Pass: '+password)
-						print('client: ',clientPort)
-						uPass = password[				
-						if password == password[userName[clientPort]]:
-							print('Password found, loading menu')
+						clientPass = data.split(" ")[5]
+						#print('Pass: '+ clientPass)
+						#print('client: ',clientPort)
+						uName = userName[clientPort]
+						uPass = password[uName]
+						
+						#if password == uPass:
+						#	print('passwords match!!!')
+
+						if clientPass == uPass:
+							#print('Password found, loading menu')
 							msgQue[clientPort] = menu
 							if s not in outputs:
-								print('adding s to outputs')
+								print('adding s to outputs: menu')
 								outputs.append(s)
-							else:
-								print('errPass')
-								msgQue[clientPort] = errPass
-								if s not in outputs:
-									print('adding s to outputs')
-									outputs.append(s)
+						else:
+							print('errPass')
+							msgQue[clientPort] = errPass
+							if s not in outputs:
+								print('errPass: adding s to outputs')
+								outputs.append(s)
+
 				elif uName in data or uName2 in data:
 					usrName = data.split(" ")[1]
 					print('In username')
-					print('Username: '+usrName)
+					#print('Username: '+usrName)
 					flag = 1	
 					for user in users:
 						if (user == usrName):
-						 	print('client: ',clientPort,' username match: ',user)	
+						 	#print('client: ',clientPort,' username match: ',user)	
 							msgQue[clientPort] = getPass
 							userName[clientPort] = user
 							if s not in outputs:
-								print('adding s to outputs')
+								#print('adding s to outputs')
 								outputs.append(s)
 							flag = 0
 							break
@@ -147,9 +167,37 @@ while inputs :
 						print('errorUserName')
 						msgQue[clientPort] = errUserName
 						if s not in outputs:
-							print('adding s to outputs')
+							print('errorUName: adding s to outputs')
 							outputs.append(s)
-				
+				elif currentPswd in data:
+					print('Checking current password before making change')
+					currentPass = data.split('\n')[3]
+					print('currPass: ',currentPass)
+
+					uName = userName[clientPort]
+					uPass = password[uName]
+
+					if currentPass == uPass:
+						print('password match, requesting new password')
+						msgQue[clientPort] = newPswdMsg
+						if s not in outputs:
+							outputs.append(s)
+					else:
+						print('error: wrong password. returning to menu')
+						msgQue[clientPort] = menu
+						if s not in outputs:
+							outputs.append(s)
+				elif newMsg in data:
+					print('Setting client ',clientPort,' new password')
+					newPass = data.strip('\n')[1]
+					#print('newPass: ',newPass)
+
+					uName = userName[clientPort]
+					password[uName] = newPass
+
+					msgQue[clientPort] = newPassSaved + menu
+					if s not in outputs:
+						outputs.append(s)
 				elif passWord in data or passWord2 in data:
 					print('inGetPass')
 					passwrd = data.split(" ")[1]
@@ -158,62 +206,79 @@ while inputs :
 					
 					uName = userName[clientPort]
 					#print('uName: ',uName)
-					
-					flag = 1	
-					for userName in password:
-						if(userName == uName):
-							if(passwrd == password[uName]):
-								print('Success! uName: ',uName,' pass: ',passwrd)	
-								msgQue[clientPort] = menu
-								
-								if s not in outputs:
-									print('adding s to outputs')
-									outputs.append(s)
-								flag = 0
-								break
-					if flag:	
+						
+					if(passwrd == password[uName]):
+						#print('Success! uName: ',uName,' pass: ',passwrd)	
+						msgQue[clientPort] = menu	
+						if s not in outputs:
+							print('adding s to outputs: menu')
+							outputs.append(s)
+					else:	
 						print('errPass')
 						msgQue[clientPort] = errPass
+						if s not in outputs:
+							print('errPass: adding s to outputs')
+							outputs.append(s)
+						
+				elif menuMsg in data:
+					print('in menu')
+					if savedMsg in data:
+						userInput = data.split('\n')[optionIndex+1]
+					else:
+						userInput = data.split('\n')[optionIndex]
+					#print('userInput: ', userInput)
+					if userInput == '1':
+						print('changing client ',clientPort,' password')
+						msgQue[clientPort] = changePswdMsg
+						if s not in outputs:
+							outputs.append(s)
+					elif userInput == '2':
+						print('Client ',clientPort,' logging out')
+						msgQue[clientPort] = loggingOutMsg
 						
 						if s not in outputs:
-							print('adding s to outputs')
 							outputs.append(s)
-				
+
 			else:
-				print('closing s')
+				#print('closing s')
 				if s in outputs:
-					outputs.remove(clientPort)
+					outputs.remove(s)
 				if s in inputs:
-					inputs.remove(clientPort)
+					inputs.remove(s)
 				s.close()
 
 				msgQue[clientPort] = ""
 		print('at end of readable')
 						
 	for s in writable:
-		print('in writable')	
+		print('in writable')
+		#print('s.getpeer: ',s.getpeername())	
 		clientPort = str(s.getpeername()[1])
-		print('client: ', clientPort)
-		actualClient = ""
+		#print('client: ', clientPort)
+		#actualClient = ""
 
-		for client in connectedClients:
-			nextMsg = msgQue[client]
-			actualClient = client
-			
-			if nextMsg:
-				msgQue[client] = ""
-			break
+		#for client in connectedClients:
+		nextMsg = msgQue[clientPort]
+		#	actualClient = client
 		
 		if(nextMsg):
-			print('sending nextMsg: ',nextMsg,' to client: ',actualClient)
-			s.send(nextMsg)
+			msgQue[clientPort] = ""
+			if loggingOutMsg in nextMsg:
+				print('Client',clientPort,' logging out')
+				s.send(nextMsg)
+				if s in inputs:
+					inputs.remove(s)
+				outputs.remove(s)
+			else:
+				print('sending nextMsg: ',nextMsg,' to client: ',clientPort)
+				s.send(nextMsg)
 		else:
-			print('no output, removing s from outputs')
+			#print('no output, removing s from outputs')
 			outputs.remove(s)
 		print('at end of writable')
 	
 	for s in exceptional:
-		print('in Exceptional, closing s')	
+		#print('in Exceptional, closing s')	
 		if s in inputs:
 			inputs.remove(s)
 		if s in outputs:
